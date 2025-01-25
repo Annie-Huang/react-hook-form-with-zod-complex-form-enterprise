94e6c636ce1c47b7b8ea1ff1a2df0e4cf32b4342
Annie Huang <annie.hn@gmail.com>
2025-01-25 22:30:10 +1100

    Create routes.tsx, dashboard-layout.tsx and add them into main.tsx. Test it can be seem on the screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { RoutesWrapper } from './routes.tsx';
 
-createRoot(document.getElementById('root')!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RoutesWrapper />
+  </StrictMode>,
+);
