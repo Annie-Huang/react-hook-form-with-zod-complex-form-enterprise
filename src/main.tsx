e5f78fc56d549dafebbcec8ae549001b32ce5ee6
Annie Huang <annie.hn@gmail.com>
2025-01-25 23:57:41 +1100

    Add MUI TemeProvider in the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RoutesWrapper } from '@/routes';
+import { ThemeProvider } from '@mui/material';
+import { theme } from '@/utils/theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RoutesWrapper />
+    <ThemeProvider theme={theme}>
+      <RoutesWrapper />
+    </ThemeProvider>
   </StrictMode>,
 );
