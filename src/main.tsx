74c9a836fdb7d4194dc059b4c29e4f552bd30e08
Annie Huang <annie.hn@gmail.com>
2025-01-26 22:17:05 +1100

    Add setupZodErrors() into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { ConfirmProvider } from '@/features/confirm/components/provider';
 import { RoutesWrapper } from '@/routes';
 import { theme } from '@/utils/theme';
-// import { setupZodErrors } from "@/utils/zodConfig";
+import { setupZodErrors } from '@/utils/zodConfig';
 import { ThemeProvider } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
@@ -11,6 +11,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const queryClient = new QueryClient();
+// show custom error message, you can comment it out and it will show the default error message when each field is invalid.
+setupZodErrors();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
