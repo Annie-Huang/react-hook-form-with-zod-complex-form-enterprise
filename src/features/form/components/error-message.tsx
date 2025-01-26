6864ffaabaf44209817e773c5e26c972cfa2dd59
Annie Huang <annie.hn@gmail.com>
2025-01-26 22:36:25 +1100

    Create a error-message.tsx to show both field error and field.root error, this is for <EducationalInstitutions> component built form useFieldArray

---
+++ src/features/form/components/error-message.tsx
@@ -0,0 +1,18 @@
+import { FormHelperText } from '@mui/material';
+
+import { ErrorMessage as RHFErrorMessage } from '@hookform/error-message';
+import { ArrayPath, FieldValues, Path } from 'react-hook-form';
+
+type ErrorMessageProps<T extends FieldValues> = {
+  name: Path<T> | ArrayPath<T>;
+};
+
+export const ErrorMessage = <T extends FieldValues>({
+  name,
+}: ErrorMessageProps<T>) => {
+  return (
+    <>
+      <RHFErrorMessage name={`${name}.root`} as={<FormHelperText error />} />
+    </>
+  );
+};
