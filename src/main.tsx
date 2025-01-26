aaaa098b3563ce56f9a1fb770c0632fc08865eec
Annie Huang <annie.hn@gmail.com>
2025-01-26 21:59:55 +1100

    Wrapped RoutesWrapper with <ConfirmProvider>

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
+import { ConfirmProvider } from '@/features/confirm/components/provider';
+import { RoutesWrapper } from '@/routes';
+import { theme } from '@/utils/theme';
+// import { setupZodErrors } from "@/utils/zodConfig";
+import { ThemeProvider } from '@mui/material';
+// import { LocalizationProvider } from "@mui/x-date-pickers";
+// import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { SnackbarProvider } from "notistack";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RoutesWrapper } from '@/routes';
-import { ThemeProvider } from '@mui/material';
-import { theme } from '@/utils/theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <RoutesWrapper />
+      <ConfirmProvider>
+        <RoutesWrapper />
+      </ConfirmProvider>
     </ThemeProvider>
   </StrictMode>,
 );
