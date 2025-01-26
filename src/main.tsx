cd221d2390848a3bf0ad6672a1bc2f56fa52b3e7
Annie Huang <annie.hn@gmail.com>
2025-01-26 22:22:20 +1100

    Add normalize.css from material ui into the app

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { ConfirmProvider } from '@/features/confirm/components/provider';
 import { RoutesWrapper } from '@/routes';
 import { theme } from '@/utils/theme';
 import { setupZodErrors } from '@/utils/zodConfig';
-import { ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -20,6 +20,10 @@ createRoot(document.getElementById('root')!).render(
       <ThemeProvider theme={theme}>
         <LocalizationProvider dateAdapter={AdapterDateFns}>
           <ConfirmProvider>
+            {/* https://mui.com/material-ui/react-css-baseline/?srsltid=AfmBOoom62aVVYrgiDnEKmcRej9MvB5yZu0zUzZGx6Ex8iv3rIdcOcyV
+                Add normalize.css to make each each browser has consistent styling with each other.
+            */}
+            <CssBaseline />
             <RoutesWrapper />
           </ConfirmProvider>
         </LocalizationProvider>
