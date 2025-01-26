42a6c1cec9361d18359693e8cec6f79abf85ec5c
Annie Huang <annie.hn@gmail.com>
2025-01-26 17:28:46 +1100

    Add EmployeeWrapper into routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,7 @@
 // import { EmployeePersonalInfo } from "@/features/employee/personal-info/page";
 // import { EmployeeReview } from "@/features/employee/review/page";
 // import { EmployeeSkills } from "@/features/employee/skills/page";
-// import { EmployeeWrapper } from "@/features/employee/wrapper/page";
+import { EmployeeWrapper } from '@/features/employee/wrapper/page';
 import { DashboardLayout } from '@/features/layout/components/dashboard-layout';
 import { BrowserRouter, Route, Routes } from 'react-router';
 
@@ -12,19 +12,19 @@ export const RoutesWrapper = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<DashboardLayout />}>
-          {/*<Route path="/employee" element={<EmployeeWrapper />}>*/}
-          {/*  <Route*/}
-          {/*    path="/employee/personal-info"*/}
-          {/*    element={<EmployeePersonalInfo />}*/}
-          {/*  />*/}
-          {/*  <Route path="/employee/history" element={<EmployeeHistory />} />*/}
-          {/*  <Route path="/employee/skills" element={<EmployeeSkills />} />*/}
-          {/*  <Route*/}
-          {/*    path="/employee/additional-info"*/}
-          {/*    element={<EmployeeAdditionalInfo />}*/}
-          {/*  />*/}
-          {/*  <Route path="/employee/review" element={<EmployeeReview />} />*/}
-          {/*</Route>*/}
+          <Route path='/employee' element={<EmployeeWrapper />}>
+            {/*  <Route*/}
+            {/*    path="/employee/personal-info"*/}
+            {/*    element={<EmployeePersonalInfo />}*/}
+            {/*  />*/}
+            {/*  <Route path="/employee/history" element={<EmployeeHistory />} />*/}
+            {/*  <Route path="/employee/skills" element={<EmployeeSkills />} />*/}
+            {/*  <Route*/}
+            {/*    path="/employee/additional-info"*/}
+            {/*    element={<EmployeeAdditionalInfo />}*/}
+            {/*  />*/}
+            {/*  <Route path="/employee/review" element={<EmployeeReview />} />*/}
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
