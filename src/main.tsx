f582158218ea334247d222db0662358c35dcd4d6
Annie Huang <annie.hn@gmail.com>
2025-01-26 22:02:26 +1100

    Need to add react-query <QueryClientProvider> in the very top level in main.tsx to be able to use react-query

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,21 @@ import { theme } from '@/utils/theme';
 import { ThemeProvider } from '@mui/material';
 // import { LocalizationProvider } from "@mui/x-date-pickers";
 // import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import { SnackbarProvider } from "notistack";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <ConfirmProvider>
-        <RoutesWrapper />
-      </ConfirmProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <ConfirmProvider>
+          <RoutesWrapper />
+        </ConfirmProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
